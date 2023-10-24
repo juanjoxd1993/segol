dad4f2870be8562a1ba7b33d1eb680770ead6023
gerardmatos <gerardmatos1990@gmail.com>
2023-10-24 16:04:06 -0500

    fix: error products liquidations

--- resources/assets/backend/js/components/LiquidationFinalModalSale.vue
+++ resources/assets/backend/js/components/LiquidationFinalModalSale.vue
@@ -291,7 +291,7 @@
                         if (this.$store.state.sales.length) {
                             this.$store.state.sales.map(item => {
                                 data.map(i => {
-                                    const art = item.details.find(e => e.article_id == i.id);
+                                    const art = item.details.find(e => e.article_id == i.id && id == item.client_id);
     
                                     if (art) {
                                         i.quantity = i.quantity - parseInt(art.quantity);
@@ -314,6 +314,8 @@
                         console.log(error.response);
                     });
                 })
+                // console.log(this.$store.state.sales)
+                // console.log(vm.articles)
 
                 this.button_text = 'Crear';
                 this.sale.client_id = 0;
