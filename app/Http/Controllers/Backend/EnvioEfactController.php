f91412a5a24497687f82793042790d3be5aba2bf
j.olivasc <j.olivasc@gmail.com>
2025-07-21 16:07:51 -0500

    actualizacion variables

--- app/Http/Controllers/Backend/EnvioEfactController.php
+++ app/Http/Controllers/Backend/EnvioEfactController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Mail;
 use Greenter\XMLSecLibs\Sunat\SignedXml;
-use App\Clients\EfactClient;
+
 
 use Auth;
 use PDF;
