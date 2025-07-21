0dfa01c1518f21d77955aaca7c446788f4b453c9
j.olivasc <j.olivasc@gmail.com>
2025-07-21 18:42:02 -0500

    correccion res borrado

--- app/Http/Controllers/Backend/EnvioEfactController.php
+++ app/Http/Controllers/Backend/EnvioEfactController.php
@@ -556,19 +556,27 @@ class EnvioEfactController extends Controller
                 $response[] = $xml_render;
 
 
+                $res = $this->billingClientPunto->sendDocumentXML(base_path('html/' . $ruta));
+
+
                
 
                 if ($res !== null) {
 
-                  
 
+
+
+                    $this->billingClientPunto->getXmlFromTicket($res['description']);
+
+                  
+/*
                     if ($item->client_email != null) {
                         Mail::to($item->client_email)->queue(new VoucherMailOficial($item));
                     }
                     if ($item->manager_mail != null) {
                         Mail::to($item->manager_mail)->queue(new VoucherMailOficial($item));
                     }
-
+*/
                     //Mail::to('juan.olivas@puntodedistribucion.com')->queue(new VoucherMailOficial($item));
                     //Mail::to('desarrollopdd@puntodedistribucion.com')->queue(new VoucherMailOficial($item));
                 }
