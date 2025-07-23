bc6c6d9342fb4f97e21d0530eb595f19b58a09eb
Angelo Cardenas <angelojcv@outlook.com>
2025-07-23 17:59:01 -0500

    fix: name file xml and exo xml

--- app/Http/Controllers/Backend/EnvioEfactController.php
+++ app/Http/Controllers/Backend/EnvioEfactController.php
@@ -541,7 +541,7 @@ class EnvioEfactController extends Controller
             }
 
             // Datos de Rutas
-            $nombre_xml = $item->company_document_number . '-' . 'F' . '-' . $item->serie_number . '-' . $item->voucher_number;
+            $nombre_xml = $item->company_document_number . '-' . $item->voucher_type_type . '-' . $item->serie_number . '-' . $item->voucher_number;
             $nombre_ruta = 'uploads/' . $item->company_short_name . '/' . $item->issue_year;
             $item->nombre_xml = $nombre_xml;
             $item->nombre_ruta = $nombre_ruta;
